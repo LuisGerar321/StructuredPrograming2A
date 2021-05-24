bc76619b619375c54d63351c1530ed343da77bc0
Luis <dojas64@gmail.com>
2021-05-24 10:51:15 -0500

    bs python done

--- unit1/project/activity_01.py
+++ unit1/project/activity_01.py
@@ -4,16 +4,18 @@ count = 2
 
 number =    int(input( "Enter a number to validate: " ))
 
+if(number == 1):
+        print("it is not a prime")
+        exit()
+
 while( count <= number  ):
         
         if( number %  count == 0 ):
-                
-                # print(count)
                 num_div += 1
-                # print("div:",  count,"num_div: ", num_div)
         count +=1
         
 if(num_div>2):
+        
         print("it is not a prime")
 else:
         print("it is a prime")
