f9f071c23d21f0e1f4648c178adca7fe37eed9bd
Luis <dojas64@gmail.com>
2021-05-31 10:53:49 -0500

    Arg main done

---
+++ unit2/arg_main.py
@@ -0,0 +1,21 @@
+import sys
+
+##Variables Globales##
+my_int = 0 
+my_str =  ""
+my_float =  0.0
+my_bool = True ##False
+my_list = [] ## list() 
+
+
+if __name__ == "__main__":
+        
+        my_int = int(  sys.argv[1]   )
+        my_str =  str(  sys.argv[2]  )
+        my_bool = bool( sys.argv[3]  )
+        my_list = sys.argv
+
+        print(   my_int * 2     , my_str*2, my_float, my_bool, my_list  )
+        
+
+
